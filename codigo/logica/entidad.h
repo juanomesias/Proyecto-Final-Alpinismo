b12e25925bd920e719241d503f3398fe356ea32c
Brayan Daniel Salazar <brayan.salazar1@udea.edu.co>
2026-05-10 22:04:05 -0500

    MODULO ENTIDAD

--- codigo/logica/entidad.h
+++ codigo/logica/entidad.h
@@ -1,10 +1,40 @@
 #ifndef ENTIDAD_H
 #define ENTIDAD_H
 
-class entidad
+class Entidad
 {
+protected:
+    float x;
+    float y;
+
+    float velocidadX;
+    float velocidadY;
+
+    // Estado de la entidad
+    bool activa;
+
 public:
-    entidad();
+    Entidad(float xInicial = 0, float yInicial = 0);
+
+    virtual ~Entidad();
+
+    virtual void actualizar();
+
+    void mover();
+
+    // Getters
+    float getX() const;
+    float getY() const;
+    float getVelocidadX() const;
+    float getVelocidadY() const;
+    bool estaActiva() const;
+
+    // Setters
+    void setX(float nuevaX);
+    void setY(float nuevaY);
+    void setVelocidadX(float nuevaVelocidadX);
+    void setVelocidadY(float nuevaVelocidadY);
+    void setActiva(bool estado);
 };
 
 #endif // ENTIDAD_H
